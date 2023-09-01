26a2ce4b74d5fcb2a9c0c5933af2e0efa705f3df
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-01 11:34:39 -0300

    inital template 🎉

---
+++ src/main/java/br/com/api/heydev/HeydevApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.heydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeydevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeydevApplication.class, args);
+	}
+
+}
